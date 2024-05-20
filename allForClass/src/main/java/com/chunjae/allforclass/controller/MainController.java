fd00da9e4186bc234778337e1d9400f3d35bf97d
loveyrooney <loveyrooney@gmail.com>
2024-05-20 18:45:49 +0900

    [fix] initial setting 수정

--- allForClass/src/main/java/com/chunjae/allforclass/controller/MainController.java
+++ allForClass/src/main/java/com/chunjae/allforclass/controller/MainController.java
@@ -1,7 +1,12 @@
 package com.chunjae.allforclass.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+    @GetMapping("/hi")
+    public String hi(){
+        return "hi";
+    }
 }
