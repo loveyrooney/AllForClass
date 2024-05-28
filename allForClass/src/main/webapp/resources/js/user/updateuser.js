1b5bfc441c2951fc0bd7a95e312a0a1ac0b6a986
bokim <bo8902@gmail.com>
2024-05-28 15:30:27 +0900

    마이페이지/강의등록 css 수정 및 메일 중복 체크 수정

--- allForClass/src/main/webapp/resources/js/user/updateuser.js
+++ allForClass/src/main/webapp/resources/js/user/updateuser.js
@@ -2,11 +2,11 @@
 let updatebtn = document.getElementById('updateuser_btn');
 
 let emailinput = document.getElementById('email');
-emailinput.addEventListener('change', function() {
+emailinput.addEventListener('change', function () {
     updatebtn.disabled = true;  // input이 변경되면 수정 버튼 비활성화
-    updatebtn.style.background='#F5F5F5';
-    updatebtn.style.border='2px solid #F5F5F5';
-    updatebtn.style.color='gray';
+    updatebtn.style.background = '#F5F5F5';
+    updatebtn.style.border = '2px solid #F5F5F5';
+    updatebtn.style.color = 'gray';
 });
 
 
@@ -15,11 +15,11 @@ document.getElementById('emailCk').onclick = function () {
     emailvalue = document.getElementById('email').value;
     console.log("Email to check: ", emailvalue); // 디버깅용 로그 추가
     fetch("/emailCheck", {
-        method   : "POST"
+        method: "POST"
         , headers: {
             'Content-Type': 'application/json',  // 요청을 JSON으로 보내기 위해 설정
         },
-        body : JSON.stringify({email: emailvalue})
+        body: JSON.stringify({email: emailvalue})
     }).then(response => {
         if (!response.ok)
             throw new Error('noooo');
@@ -35,32 +35,38 @@ document.getElementById('emailCk').onclick = function () {
         let checkspan = document.createElement('span');
         checkspan.id = 'email-check-span';
 
-        if (data != 0 && emailvalue != userEmail) { //사용 불가능한 이메일
+        let checktext;
 
-            let alreadytext = document.createTextNode('이미 존재하는 이메일입니다.');
-            checkspan.appendChild(alreadytext);
-            checkspan.style.color = "red";
+        if (emailvalue == "" || emailvalue == null || (emailvalue != userEmail && data != 0)) {
+            if(emailvalue == "" || emailvalue == null){  //값이 없을 때
+                checktext = document.createTextNode('이메일 주소를 입력해주세요.');
+            } else if (emailvalue != userEmail && data != 0) { //중복값으로 사용 불가능한 이메일
 
-            updatebtn.disabled=true; // 등록 버튼 비활성화
-            updatebtn.style.background='#F5F5F5';
-            updatebtn.style.border='2px solid #F5F5F5';
-            updatebtn.style.color='gray';
+                checktext = document.createTextNode('이미 존재하는 이메일입니다.');
+            }
+                checkspan.appendChild(checktext);
+                checkspan.style.color = "red";
 
-            // 메일 입력창 활성화
-            emailinput.readOnly = false;
-            emailinput.style.removeProperty('background');
+                updatebtn.disabled = true; // 등록 버튼 비활성화
+                updatebtn.style.background = '#F5F5F5';
+                updatebtn.style.border = '2px solid #F5F5F5';
+                updatebtn.style.color = 'gray';
+
+                // 메일 입력창 활성화
+                emailinput.readOnly = false;
+                emailinput.style.removeProperty('background');
 
         } else {
-            let oktext;
+
             if (emailvalue == userEmail) { //입력값과 기존 이메일이 같을 때
-                oktext = document.createTextNode('현재 사용중인 이메일 입니다.');
+                checktext = document.createTextNode('현재 사용중인 이메일 입니다.');
             } else {
-                oktext = document.createTextNode('사용 가능한 이메일입니다.');
+                checktext = document.createTextNode('사용 가능한 이메일입니다.'); //사용가능
             }
 
             emailinput.readOnly = true; // 다른 텍스트로 입력 못하도록 입력창 비활성화
             emailinput.style.background = '#F5F5F5';
-            checkspan.appendChild(oktext);
+            checkspan.appendChild(checktext);
             checkspan.style.color = "green";
 
             updatebtn.disabled = false; //수정하기 버튼 활성화
@@ -84,7 +90,7 @@ document.getElementById('emailCk').onclick = function () {
 // 삭제 버튼 클릭 이벤트 리스너 추가
 const deleteUserBtn = document.getElementById('deleteuser_btn');
 
-deleteUserBtn.addEventListener('click', function() {
+deleteUserBtn.addEventListener('click', function () {
 
     let uid = document.getElementById('uid').value;
     const userConfirmed = confirm('회원 탈퇴하시겠습니까?');
@@ -92,7 +98,7 @@ deleteUserBtn.addEventListener('click', function() {
     // 사용자가 확인 버튼을 눌렀을 때
     if (userConfirmed) {
 
-        fetch('/deleteuser/'+uid, {
+        fetch('/deleteuser/' + uid, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
@@ -100,9 +106,9 @@ deleteUserBtn.addEventListener('click', function() {
         })
             .then(response => response.json())
             .then(data => {
-                if (data ===1) {
+                if (data === 1) {
                     alert('회원 탈퇴가 완료되었습니다.');
-                    if(role == 'admin'){
+                    if (role == 'admin') {
                         location.href = '/admin';
                     } else {
                         location.href = '/logout';
@@ -126,7 +132,7 @@ deleteUserBtn.addEventListener('click', function() {
 // 취소 버튼
 let updateresetbtn = document.getElementById('updatereset_btn');
 
-updateresetbtn.addEventListener('click', function() {
+updateresetbtn.addEventListener('click', function () {
     // 메일 입력창 활성화
     emailinput.readOnly = false;
     emailinput.style.removeProperty('background');
